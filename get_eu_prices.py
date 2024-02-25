660ad83bdd889ff369f435ceaabd320615295acc
MaltaWebDev <MaltaWebDev@gmail.com>
2024-02-25 14:12:18 +0100

    Complete add_exchange_to_ticker function
    At the moment, the screener doesn't work as the tickers are incomplete.
    For example, if the stock trades on the LSE, its ticker should end in .L
    Add logic to function to add ticker extension based on exchange

--- get_eu_prices.py
+++ get_eu_prices.py
@@ -8,6 +8,54 @@ warnings.filterwarnings('ignore', category=FutureWarning, module='yfinance.utils
 def add_exchange_to_ticker(symbol, exchange):
     if exchange == 'XETRA':
         return symbol + '.DE'
+    if exchange == 'EURONEXT - EURONEXT BRUSSELS':
+        return symbol + '.BR'
+    if exchange == 'EURONEXT - EURONEXT AMSTERDAM':
+        return symbol + '.AS'
+    if exchange == 'BOLSA DE MADRID':
+        return symbol + '.MC'
+    if exchange == 'ATHENS EXCHANGE S.A. CASH MARKET':
+        return symbol + '.AT'
+    if exchange == 'BUDAPEST STOCK EXCHANGE':
+        return symbol + 'BUD.'
+    if exchange == 'ELECTRONIC SHARE MARKET':
+        return symbol
+    if exchange == 'EURONEXT - EURONEXT PARIS':
+        return symbol + '.PA'
+    if exchange == 'EURONEXT GROWTH PARIS':
+        return symbol + '.PA'
+    if exchange == 'FIRST NORTH FINLAND - SME GROWTH MARKET':
+        return symbol + '.HE'
+    if exchange == 'FIRST NORTH SWEDEN - SME GROWTH MARKET':
+        return symbol + '.ST'
+    if exchange == 'HANSEATISCHE WERTPAPIERBOERSE HAMBURG':
+        return symbol + '.HM'
+    if exchange == 'IRISH STOCK EXCHANGE - ALL MARKET':
+        return symbol + '.IR'
+    if exchange == 'LJUBLJANA STOCK EXCHANGE (OFFICIAL MARKET)':
+        return symbol + '.LJ'
+    if exchange == 'MOSCOW EXCHANGE - ALL MARKETS':
+        return symbol + '.ME'
+    if exchange == 'NASDAQ COPENHAGEN A/S':
+        return symbol + '.CO'
+    if exchange == 'LONDON STOCK EXCHANGE':
+        return symbol + '.L'
+    if exchange == 'NASDAQ HELSINKI LTD':
+        return symbol + '.HE'
+    if exchange == 'NASDAQ ICELAND HF.':
+        return symbol + '.IC'
+    if exchange == 'NASDAQ STOCKHOLM AB':
+        return symbol + '.ST'
+    if exchange == 'OPERADOR DE MERCADO IBERICO DE ENERGIA - PORTUGAL':
+        return symbol + '.LS'
+    if exchange == 'OSLO BORS ASA':
+        return symbol + '.OL'
+    if exchange == 'PRAGUE STOCK EXCHANGE':
+        return symbol + '.PR'
+    if exchange == 'SIX SWISS EXCHANGE':
+        return symbol + '.SW'
+    if exchange == 'WARSAW STOCK EXCHANGE/EQUITIES/MAIN MARKET':
+        return symbol + '.WA'
     else:
         return symbol
 
